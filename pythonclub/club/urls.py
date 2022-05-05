fb20d68bac14b5bf09a264ab32c7cd178823332c
emballar <erinmichelle.ballar@gmail.com>
2022-05-05 13:33:24 -0700

    assignment 6 commit

--- pythonclub/club/urls.py
+++ pythonclub/club/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
+    path('meetings/', views.meetings, name='meetings'),
+    path('meetingsdetail/<int:id>', views.meetingdetail, name='detail'),
     path('resources/', views.resources, name='resources'),
 ]
--- pythonclub/club/views.py
+++ pythonclub/club/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
-from .models import Resource
+from django.shortcuts import render, get_object_or_404
+from .models import Meeting, Resource
+from django.urls import reverse_lazy
 # Create your views here.
 def index(request):
     return render(request, 'club/index.html')
+
+def meetings(request):
+    meeting_list=Meeting.objects.all()
+    return render(request, 'club/meetings.html',{'meeting_list': meeting_list})
+
+def meetingdetail(request, id):
+    meeting=get_object_or_404(Meeting, pk=id)
+    return render(request, 'club/meetingdetail.html', {'meeting' : meeting })
 
 def resources(request):
     resource_list=Resource.objects.all()
